b8da24173c967dc1577ebcc24ae6873064907643
Cedric ROMAN <roman@numengo.com>
2018-07-01 18:44:34 +0200

    re-add __init__.py correct non utf-8 character in LICENSE
    Signed-off-by: Cedric ROMAN <roman@numengo.com>

---
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.package_name}}/__init__.py
@@ -0,0 +1,22 @@
+__version__ = '{{ cookiecutter.version }}'
+{%- if cookiecutter.c_extension_support == 'cffi' %}
+
+from .{{ cookiecutter.c_extension_module }} import ffi as _ffi
+from .{{ cookiecutter.c_extension_module }} import lib as _lib
+
+
+def {{ cookiecutter.c_extension_function }}(args):
+    args = [_ffi.new('char[]', arg) for arg in args]
+    result = _lib.{{ cookiecutter.c_extension_function }}(len(args), _ffi.new('char *[]', args))
+    if result == _ffi.NULL:
+        return ''
+    else:
+        return _ffi.string(result)
+{%- elif cookiecutter.c_extension_support != 'no' %}
+
+from .{{ cookiecutter.c_extension_module }} import {{ cookiecutter.c_extension_function }}  # noqa
+{%- endif %}
+
+__author__ = '{{ cookiecutter.full_name }}'
+__email__ = '{{ cookiecutter.email }}'
+__version__ = '{{ cookiecutter.version }}'
